3ced32af7201fae68e05374d55dbdfd2cea99c8d
Someone-Unknown69 <kartikchhatre8@gmail.com>
2025-06-30 12:11:32 +0530

    debugging login

--- api/login.js
+++ api/login.js
@@ -3,10 +3,14 @@ import querystring from "querystring";
 export default function handler(req, res) {
   const redirect_uri = "https://vibefy-theta.vercel.app/api/callback"; // change this!
   const scope = "user-read-private user-read-email user-top-read";
-
+  const clientId = process.env.CLIENT_ID;
+  if (!clientId) {
+    res.status(500).send("CLIENT_ID environment variable is not set.");
+    return;
+  }
   const authUrl = "https://accounts.spotify.com/authorize?" + querystring.stringify({
     response_type: "code",
-    client_id: process.env.CLIENT_ID,
+    client_id: clientId,
     scope,
     redirect_uri
   });
