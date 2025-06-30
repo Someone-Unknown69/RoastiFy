9bd8ad559c631902807c775620fbf6859dc4e46b
Someone-Unknown69 <kartikchhatre8@gmail.com>
2025-06-30 16:13:11 +0530

    changin ai

--- api/report.js
+++ api/report.js
@@ -21,24 +21,13 @@ export default async function handler(req, res) {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-    "model": "google/gemini-2.5-pro",
-    "messages": [
+      "model": "openai/gpt-4o",
+      "messages": [
       {
         "role": "user",
-        "content": [
-          {
-            "type": "text",
-            "text": "What is in this image?"
-          },
-          {
-            "type": "image_url",
-            "image_url": {
-              "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-            }
-          }
-        ]
+        "content": "Roast this Spotify playlist: " + JSON.stringify(tracks)
       }
-    ]
+  ]
   })
   });
 
