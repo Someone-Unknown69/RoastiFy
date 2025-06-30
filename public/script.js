86eeaa039bf197449e90a213099ee06b4dcbe4a1
Someone-Unknown69 <kartikchhatre8@gmail.com>
2025-06-30 12:44:10 +0530

    testing of top artist

--- public/script.js
+++ public/script.js
@@ -1,3 +1,22 @@
 const token = new URLSearchParams(window.location.search).get('token');
 
-console.log(`<p>Access Token:</p><code>${token}</code>`);
+console.log(token);
+
+
+async function getInfo(token) {
+    try{
+        const request = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=10', {
+        headers: { Authorization: `Bearer ${token}` }
+        });
+
+        const data = await request.json();
+    } catch(err) {
+        console.log("Failed to fetch info")
+    }
+
+    document.body.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+    
+}
+if (token) {
+    getInfo(token)
+}
