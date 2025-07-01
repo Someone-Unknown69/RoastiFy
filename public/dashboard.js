3a51ada93533470d51f714269194cbd5b6d5a1e5
Someone-Unknown69 <kartikchhatre8@gmail.com>
2025-07-01 15:06:45 +0530

    multi page speed test

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,22 +1,64 @@
+const pages = document.querySelectorAll('.Output-container')
+const next_btn = document.getElementById("next");
+const prev_btn = document.getElementById("prev")
 const tracks = JSON.parse(localStorage.getItem("tracks") || "[]");
 const container = document.getElementById("tracks-container");
+let curr_page = 0;
+
+function check_btn(curr_page) {
+  if(curr_page === 0 && prev_btn){
+    prev_btn.style.display = "none";
+  } else {
+    prev_btn.style.display = "flex";
+  }
+
+  if(curr_page === 5 && next_btn){
+    next_btn.style.display = "none";
+  } else {
+    next_btn.style.display = "flex";
+  }
+}
+
+function next_page(curr_page) {
+  pages[curr_page].style.display = "none";
+  pages[curr_page + 1].style.display = "flex";
+  return curr_page + 1;
+}
+function prev_page(curr_page) {
+  pages[curr_page].style.display = "none";
+  pages[curr_page - 1].style.display = "flex";
+  return curr_page - 1;
+}
+
+next_btn.addEventListener("click",() => {
+  curr_page = next_page(curr_page)
+  check_btn(curr_page);
+})
+
+prev_btn.addEventListener("click",() => {
+  curr_page = prev_page(curr_page);
+  check_btn(curr_page);
+})
+
+
 
 if (tracks.length === 0) {
-  container.textContent = "No tracks found.";
+  pages[0].textContent = "No tracks found.";
 } else {
   async function getReport(tracks) {
     const response = await fetch("/api/report", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ tracks })
-    });
+    });  
     const data = await response.json();
     return data;
-  }
+  }  
 
+  // Vibe-Psychoanalysis
   getReport(tracks).then(aiResult => {
-    container.innerHTML = aiResult.message; // Render the AI's HTML report
+    pages[0].innerHTML = aiResult.message; // Render the AI's HTML report
   }).catch(err => {
-    container.textContent = "AI request failed: " + err.message;
-  });
-}
+    pages[0].textContent = "AI request failed: " + err.message;
+  });  
+}  
